4b9e56e2b2b34a3aeffbe87d07b9ce5178735bab
Адель <@DESKTOP-BMDLKTB>
2023-02-07 12:11:51 +0300

    Добавьте файлы проекта.

---
+++ Forms1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forms1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = true;
+        }
+
+        private void textbox1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            button2.Visible = false;
+            textBox1.BackColor = Color.White;
+        }
+    }
+}
